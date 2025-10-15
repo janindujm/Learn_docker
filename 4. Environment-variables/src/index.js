5350b11b3405215b5065a09a7ce51873225be2c3
janindujm <janindujm@gmail.com>
2025-10-15 09:21:01 +0530

    docker with Env file

--- 4. Environment-variables/src/index.js
+++ 4. Environment-variables/src/index.js
@@ -3,9 +3,11 @@ const app = express();
 const PORT = process.env.PORT ;
 
 app.get('/', (req, res) => {
-    res.send(`Hello! Your application is running on port ${PORT}`);
+    res.send(`Hello! Your ${process.env.App_NAME} is running on port ${PORT}`);
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+//curl http://localhost:5000
